1d36ba5a069392fac6548fca8892bd14b1fa7631
krzyczak <krzyczak@users.noreply.github.com>
2023-04-28 15:28:25 +0100

    chore: Add nvim-autopairs plugin

--- lua/krzyczak/packer.lua
+++ lua/krzyczak/packer.lua
@@ -61,4 +61,9 @@ return require('packer').startup(function(use)
       {'L3MON4D3/LuaSnip'},     -- Required
     }
   }
+
+  use {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
 end)
