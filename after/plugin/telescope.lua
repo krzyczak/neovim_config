a139e2cdd62ec7f3dd9810b861764aaa30e6c4c7
krzyczak <krzyczak@users.noreply.github.com>
2023-05-02 15:37:01 +0100

    chore: Add buffers switch

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
 
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
