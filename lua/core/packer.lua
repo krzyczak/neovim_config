128d05624d68d5ba16290c4f815d7c9f54afa407
krzyczak <krzyczak@users.noreply.github.com>
2023-05-29 18:44:56 +0100

    chore: add nvim-lualine/lualine.nvim plugin

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -35,6 +35,11 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
+  use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
